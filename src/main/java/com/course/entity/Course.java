60faae01466887e232879392fb44cce78275a204
Gaurav Singh <grvsgh7@gmail.com>
2022-02-18 21:39:01 +0530

    update course db

--- src/main/java/com/course/entity/Course.java
+++ src/main/java/com/course/entity/Course.java
@@ -1,6 +1,8 @@
 package com.course.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.ToString;
 @Entity
 public class Course {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String title;
 	private String description;
